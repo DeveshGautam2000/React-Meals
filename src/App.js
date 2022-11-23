8907469f4ea420a884d83155df9dd5a5fbe60221
DeveshGautam2000 <deveshgautamking@gmail.com>
2022-11-23 13:34:19 +0530

    Meal Item and MealItem Form Done

--- src/App.js
+++ src/App.js
@@ -1,9 +1,13 @@
 import React from "react";
 import Header from "./components/Layouts/Header";
+import Meals from "./components/Meals/Meals";
 function App() {
   return (
     <React.Fragment>
       <Header />
+      <main>
+        <Meals />
+      </main>
     </React.Fragment>
   );
 }
